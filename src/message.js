e68a34ebf32f7d73ce7c822e75817ac3d91abea5
Augusto99MK <carlosaugustox6@gmail.com>
2024-03-14 16:07:06 -0400

    somente o sistema bot

--- src/message.js
+++ src/message.js
@@ -5,9 +5,6 @@ const MenuModel = require("./models/menu_model");
 module.exports = async (client) => {
   await connectToDatabase();
 
-  // Adicione uma variável para armazenar o estado de espera pelo nome
-  let waitingForName = false;
-
   client.on("message", async (message) => {
     const phoneNumber = message.from;
 
@@ -20,16 +17,12 @@ module.exports = async (client) => {
         phoneNumber,
       });
       await clientData.save();
-    }
 
-    // Se o bot está aguardando o nome do usuário, salve o nome no banco de dados
-    if (waitingForName) {
-      clientData.name = message.body;
-      await clientData.save(); // Salva o nome no banco de dados
       await client.sendMessage(
         phoneNumber,
-        `Olá, ${clientData.name}! Como posso ajudá-lo hoje?`
+        "Olá! Seja bem-vindo ao nosso sistema de chat automatizado. Eu sou o botzap, seu guia turístico. Irei te auxiliar a escolher hotéis, restaurantes ou pontos turísticos da cidade."
       );
+
       // Exibir menu de opções
       const menuOptions = await MenuModel.find({});
       let menuText = "Por favor, escolha uma opção:\n";
@@ -37,14 +30,6 @@ module.exports = async (client) => {
         menuText += `${option.optionNumber} - ${option.description}\n`;
       });
       await client.sendMessage(phoneNumber, menuText);
-      waitingForName = false; // Redefine o estado para não aguardar mais o nome
-    } else {
-      // Se não está aguardando o nome, solicite o nome
-      await client.sendMessage(
-        phoneNumber,
-        "Olá! Seja bem-vindo ao nosso sistema de chat automatizado, para prosseguirmos por favor, me informe o seu nome.😊"
-      );
-      waitingForName = true; // Define o estado para aguardar o nome
     }
   });
 };
